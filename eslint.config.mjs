4cb8403bc62cd47589cbe8934d9333ce24600c54
Bac Nguyen <nxbac.dev@gmail.com>
2025-03-14 18:02:25 +0700

    feat(base): eslint + prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,146 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  {
+    ignores: ["eslint.config.mjs"],
+  },
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@next/next/recommended",
+  ),
+  {
+    plugins: {
+      react,
+      "@typescript-eslint": typescriptEslint,
+      prettier,
+      "simple-import-sort": simpleImportSort,
+    },
 
-export default eslintConfig;
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        project: "./tsconfig.json",
+      },
+    },
+
+    rules: {
+      // these configs are temporary solution for issue in eslint new version: https://github.com/eslint/eslint/discussions/18858
+      "@typescript-eslint/lines-between-class-members": "off",
+      "@typescript-eslint/no-throw-literal": "off",
+
+      "@typescript-eslint/no-use-before-define": "off",
+      "import/extensions": 0,
+      "import/no-unresolved": 0,
+
+      "prettier/prettier": [
+        "error",
+        {
+          arrowParens: "always",
+          bracketSameLine: false,
+          bracketSpacing: true,
+          semi: false,
+          experimentalTernaries: false,
+          singleQuote: true,
+          jsxSingleQuote: false,
+          quoteProps: "as-needed",
+          trailingComma: "all",
+          singleAttributePerLine: false,
+          htmlWhitespaceSensitivity: "css",
+          vueIndentScriptAndStyle: false,
+          proseWrap: "preserve",
+          insertPragma: false,
+          printWidth: 100,
+          requirePragma: false,
+          tabWidth: 2,
+          useTabs: false,
+          embeddedLanguageFormatting: "auto",
+        },
+      ],
+
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^@?\\w"],
+            ["^\\u0000"],
+            ["^(@/components/layouts)(/.*|$)"],
+            [
+              "^(@/components/features)(/.*|$)",
+              "^(@/components/common)(/.*|$)",
+              "^(@/components/ui)(/.*|$)",
+            ],
+            ["^(@)(/.*|$)"],
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            ["^.+\\.(css)$", "^.+\\.(scss)$"],
+          ],
+        },
+      ],
+
+      "import/prefer-default-export": 0,
+      "import/no-extraneous-dependencies": 0,
+      "react/require-default-props": 0,
+      "react/react-in-jsx-scope": 0,
+      "react/jsx-props-no-spreading": 0,
+    },
+  },
+  {
+    files: ["./src/components/ui/**/*.{js,jsx,ts,tsx}"],
+
+    rules: {
+      "react/jsx-props-no-spreading": 0,
+      "react/prop-types": 0,
+      "no-use-before-define": 0,
+      "jsx-a11y/heading-has-content": 0,
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/no-shadow": 0,
+      "@typescript-eslint/no-unused-vars": 0,
+      "react/jsx-no-constructed-context-values": 0,
+    },
+  },
+  {
+    files: [
+      "./src/components/common/**/*.{js,jsx,ts,tsx}",
+      "./src/components/form/**/*.{js,jsx,ts,tsx}",
+    ],
+
+    rules: {
+      "react/jsx-props-no-spreading": 0,
+    },
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -14,20 +15,18 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+            Get started by editing{' '}
+            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
               src/app/page.tsx
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
+          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex flex-col items-center gap-4 sm:flex-row">
           <Link
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
@@ -53,20 +52,14 @@ export default function Home() {
           </Link>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
           Learn
         </a>
         <a
@@ -75,13 +68,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
           Examples
         </a>
         <a
@@ -90,16 +77,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
           Go to nextjs.org →
         </a>
       </footer>
     </div>
-  );
+  )
 }
