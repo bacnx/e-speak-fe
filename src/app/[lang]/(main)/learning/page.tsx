86edff784b331b608389025b7cdd5b7b7eadd3f4
Bac Nguyen <nxbac.dev@gmail.com>
2025-03-29 22:38:34 +0700

    feat(lesson): demo lesson on learning page

--- src/app/[lang]/(main)/learning/page.tsx
+++ src/app/[lang]/(main)/learning/page.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+
 import { DefaultPageProps } from '@/types/common'
 
 export default async function LearningPage({ params }: DefaultPageProps) {
-  return <div>Learning Page</div>
+  return (
+    <div className="container mx-auto w-full px-4 py-2">
+      <h2 className="text-2xl font-semibold">Learning Page</h2>
+      <Link href="/lesson">
+        <Button variant="link">Lesson demo</Button>
+      </Link>
+    </div>
+  )
 }
--- src/components/feature/lesson/index.tsx
+++ src/components/feature/lesson/index.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useState } from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { XIcon } from 'lucide-react'
 
 import { Progress } from '@/components/ui/progress'
 
@@ -17,16 +19,27 @@ interface LessonProps {
 export default function Lesson({ dictionary, data }: LessonProps) {
   const [current, setCurrent] = useState(0)
   const onPrevious = () => setCurrent(Math.max(current - 1, 0))
-  const onNext = () => setCurrent(Math.min(current + 1))
+  const onNext = () => setCurrent(Math.min(current + 1, data.length - 1))
   const progress = Math.round(((current + 1) / data.length) * 100)
 
   if (data.length === 0) {
     return <div>No data</div>
   }
 
+  const onBack = () => {
+    window.location.href = '/learning'
+  }
+
   return (
-    <div className="py-4">
-      <Progress value={progress} />
+    <div className="flex flex-col items-center py-4">
+      <div className="flex w-full items-center justify-between gap-4">
+        <button type="button" className="cursor-pointer text-gray-400" onClick={onBack}>
+          <XIcon className="h-7 w-7" />
+        </button>
+        <Progress value={progress} />
+        <span className="p-1 text-2xl font-semibold text-gray-400">{progress}%</span>
+      </div>
+
       <VocabularyCard
         dictionary={dictionary}
         data={data[current]}
